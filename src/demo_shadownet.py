6f81d5cf2e8d9195d6237c3dff8561184f43d03b
HubertLegec <hubert.legec@gmail.com>
2018-07-28 17:47:50 +0200

    demo_shadownet - refactoring

--- src/demo_shadownet.py
+++ src/demo_shadownet.py
@@ -4,44 +4,33 @@ import numpy as np
 import cv2
 import argparse
 import matplotlib.pyplot as plt
-try:
-    from cv2 import cv2
-except ImportError:
-    pass
-
 from crnn_model import ShadowNet
 from global_configuration import config
-from local_utils import init_logger, data_utils
-from tensorflow.python.framework import graph_util, graph_io
+from utils import init_logger, TextFeatureIO, load_and_resize_image
 
 logger = init_logger()
 
 
-def init_args():
+def parse_params():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image_path', type=str, help='Where you store the image',
-                        default='data/test_images/test_01.jpg')
-    parser.add_argument('--weights_path', type=str, help='Where you store the weights',
-                        default='model/shadownet/shadownet_2017-09-29-19-16-33.ckpt-39999')
-
+    parser.add_argument('-i', '--image_path', type=str, help='Path to image file')
+    parser.add_argument('-w', '--weights_path', type=str, help='Where you store the weights')
     return parser.parse_args()
 
 
 def recognize(image_path, weights_path, is_vis=True):
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
-    image = cv2.resize(image, (100, 32))
-    image = np.expand_dims(image, axis=0).astype(np.float32)
+    image = load_and_resize_image(image_path)
 
     inputdata = tf.placeholder(dtype=tf.float32, shape=[1, 32, 100, 3], name='input')
 
-    net = crnn_model.ShadowNet(phase='Test', hidden_nums=256, layers_nums=2, seq_length=25, num_classes=37)
+    net = ShadowNet(phase='Test', hidden_nums=256, layers_nums=2, seq_length=25, num_classes=37)
 
     with tf.variable_scope('shadow'):
         net_out = net.build_shadownet(inputdata=inputdata)
 
     decodes, _ = tf.nn.ctc_beam_search_decoder(inputs=net_out, sequence_length=25*np.ones(1), merge_repeated=False)
 
-    decoder = data_utils.TextFeatureIO()
+    decoder = TextFeatureIO()
 
     # config tf session
     sess_config = tf.ConfigProto()
@@ -54,13 +43,9 @@ def recognize(image_path, weights_path, is_vis=True):
     sess = tf.Session(config=sess_config)
 
     with sess.as_default():
-
         saver.restore(sess=sess, save_path=weights_path)
-
         preds = sess.run(decodes, feed_dict={inputdata: image})
-
         preds = decoder.writer.sparse_tensor_to_str(preds[0])
-
         logger.info('Predict image {:s} label {:s}'.format(ops.split(image_path)[1], preds[0]))
 
         if is_vis:
@@ -70,14 +55,11 @@ def recognize(image_path, weights_path, is_vis=True):
 
         sess.close()
 
-    return
-
 
 if __name__ == '__main__':
-    # Inti args
-    args = init_args()
-    if not ops.exists(args.image_path):
-        raise ValueError('{:s} doesn\'t exist'.format(args.image_path))
-
-    # recognize the image
-    recognize(image_path=args.image_path, weights_path=args.weights_path)
+    params = parse_params()
+    image_path = params.image_path
+    weights_path = params.weights_path
+    if not ops.exists(image_path):
+        raise ValueError("{} doesn't exist".format(image_path))
+    recognize(image_path=image_path, weights_path=weights_path)
