c118c72c09498d0fb852ce5bf6204e8ecc2c6072
HubertLegec <hubert.legec@gmail.com>
2018-08-18 01:41:47 +0200

    ICDAR TFRecords builder

--- src/data_provider/icdar_tf_records_builder.py
+++ src/data_provider/icdar_tf_records_builder.py
@@ -1,46 +1,51 @@
-import numpy as np
 import tensorflow as tf
+import numpy as np
 from tqdm import tqdm
-from os.path import join, dirname, exists
-from utils import FeatureIO
 from logger import LogFactory
-from . import ImageDescription, Lexicon
+from os import path
+from utils import FeatureIO
+from . import ImageDescription
 
 
-class TfRecordBuilder:
-    def __init__(self, annotations_file, output_file, lexicon: Lexicon, validate_paths=False):
+class IcdarTfRecordsBuilder:
+    def __init__(self, annotations_file, output_file):
         self._log = LogFactory.get_logger()
         self._annotations_file = annotations_file
         self._output_file = output_file
-        self._validate = validate_paths
         self._encoder = FeatureIO()
-        self._lexicon = lexicon
 
     def process(self):
-        if not exists(self._annotations_file):
+        if not path.exists(self._annotations_file):
             raise IOError(f"File {self._annotations_file} doesn't exists")
-        annotations_dir = dirname(self._annotations_file)
+        annotations_dir = path.dirname(self._annotations_file)
         self._log.info("Reading annotations file...")
         with open(self._annotations_file, 'r') as anno_file:
-            infos = np.array([tmp.strip().split() for tmp in anno_file.readlines()])
+            infos = np.array([tmp.strip().split(', ') for tmp in anno_file.readlines()])
         self._log.info(f"Number of rows in annotation file: {infos.shape[0]}")
-        if self._validate:
-            self._validate_paths(annotations_dir, infos)
 
         self._log.info("Processing...")
+        skipped = 0
         with tf.python_io.TFRecordWriter(self._output_file) as writer:
             for info in tqdm(infos):
                 try:
                     self.process_item(writer, annotations_dir, info)
-                except Exception:
+                except Exception as e:
                     self._log.warn(f"File {info[0]} skipped")
+                    skipped += 1
+        self._log.info("Skipped count: {}".format(skipped))
 
     def process_item(self, writer, annotations_dir, info):
-        label = self._lexicon.get_word_by_index(int(info[1]))
+        label = info[1][1:-1].lower()
         descr = ImageDescription(info[0], annotations_dir, label)
-        descr.encode_label(lambda character: self._encoder.char_to_int(character))
+        descr.encode_label(lambda character: self.char_to_int(character))
         feat_descr = self.create_feature_example(descr)
         writer.write(feat_descr)
+
+    def char_to_int(self, character):
+        encoded = self._encoder.char_to_int(character)
+        if encoded > 36:
+            raise ValueError("Unsupported character")
+        return encoded
 
     @classmethod
     def create_feature_example(cls, img_descr):
@@ -51,14 +56,3 @@ class TfRecordBuilder:
         })
         example = tf.train.Example(features=features)
         return example.SerializeToString()
-
-    def _validate_paths(self, annotations_dir, infos):
-        self._log.info("Check if paths are valid...")
-        inv_paths = [p for p in infos[:, 0] if not exists(join(annotations_dir, p))]
-        if len(inv_paths) > 0:
-            print(' --- DOES NOT EXISTS --- ')
-            for p in inv_paths:
-                print(p)
-            raise ValueError("Invalid images")
-
-
