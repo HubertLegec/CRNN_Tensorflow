3265c65093a6883a6faa3ba2c78ad9ada86a11be
HubertLegec <hubert.legec@gmail.com>
2018-09-09 19:46:55 +0200

    Levenshtein distance calculation - cleanup

--- src/utils/levenshtein_distance.py
+++ src/utils/levenshtein_distance.py
@@ -8,42 +8,33 @@ def batch_levenshtein_distance(predictions, labels) -> list:
     return distances
 
 
-def levenshtein_distance(s, t, costs=(1, 1, 1)):
+def levenshtein_distance(s: str, t: str):
     """
-        iterative_levenshtein(s, t) -> ldist
-        ldist is the Levenshtein distance between the strings
-        s and t.
+        Calculates the Levenshtein distance between the strings s and t.
         For all i and j, dist[i,j] will contain the Levenshtein
-        distance between the first i characters of s and the
-        first j characters of t
-
-        costs: a tuple or a list with three integers (d, i, s)
-               where d defines the costs for a deletion
-                     i defines the costs for an insertion and
-                     s defines the costs for a substitution
+        distance between the first i characters of s and the first j characters of t
 
         source: https://www.python-course.eu/levenshtein_distance.php
     """
     rows = len(s) + 1
     cols = len(t) + 1
-    deletes, inserts, substitutes = costs
 
     dist = [[0 for _ in range(cols)] for _ in range(rows)]
     # source prefixes can be transformed into empty strings by deletions:
     for row in range(1, rows):
-        dist[row][0] = row * deletes
+        dist[row][0] = row
     # target prefixes can be created from an empty source string by inserting the characters
     for col in range(1, cols):
-        dist[0][col] = col * inserts
+        dist[0][col] = col
 
     for col in range(1, cols):
         for row in range(1, rows):
             if s[row - 1] == t[col - 1]:
                 cost = 0
             else:
-                cost = substitutes
-            dist[row][col] = min(dist[row - 1][col] + deletes,
-                                 dist[row][col - 1] + inserts,
+                cost = 1
+            dist[row][col] = min(dist[row - 1][col] + 1,
+                                 dist[row][col - 1] + 1,
                                  dist[row - 1][col - 1] + cost)  # substitution
 
     return dist[rows - 1][cols - 1]
