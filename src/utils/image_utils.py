3141a9ca5c0621c4d8fc8cea179ef56b4f6c20e3
HubertLegec <hubert.legec@gmail.com>
2018-09-14 18:46:20 +0200

    load image in grayscale

--- src/utils/image_utils.py
+++ src/utils/image_utils.py
@@ -3,6 +3,17 @@ import numpy as np
 
 
 def load_and_resize_image(image_path: str):
-    image = cv2.imread(image_path, cv2.IMREAD_COLOR)
+    """
+    Loads image as gray scale and scales to size `100 x 32`
+    :param image_path: Path to image file
+    :return: Numpy array with gray scale image of size `100 x 32`
+    """
+    image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     image = cv2.resize(image, (100, 32))
-    return np.expand_dims(image, axis=0).astype(np.float32)
+    equ = cv2.equalizeHist(image)
+    img2 = np.zeros((32, 100, 3))
+    img2[:, :, 0] = equ
+    img2[:, :, 1] = equ
+    img2[:, :, 2] = equ
+    return np.expand_dims(img2, axis=0).astype(np.float32)
+
